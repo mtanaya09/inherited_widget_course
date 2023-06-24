eceb960dae0aeed36e482d08a6248ade492ef533
mtanaya09 <mtanaya09@gmail.com>
2023-06-24 21:51:32 +0800

    init Inherited Widget Course

---
+++ android/app/src/main/kotlin/com/example/inherited_widget_course/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.inherited_widget_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+      home: const HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home KEL'),
+      ),
+    );
+  }
+}
